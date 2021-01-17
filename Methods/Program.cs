22fffd9b541cca6fd875efba5f5660bf76c63ab1
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-18 00:04:33 +0300

    referans tip eklendi

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,13 +10,19 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            int result= Topla(5,8);
+            int number1 = 5;
+            int number2 = 9;
+
+
+            int result= Topla(ref number1,number2);
             Console.WriteLine(result);
+            
 
         }
-        static int  Topla(int number1, int number2)
+        static int  Topla(ref int number1, int number2)
         {
             int result;
+          number1 = 30;
             return result= number1 + number2;
         }
     }
